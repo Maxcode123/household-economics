07e61c025eb1e5c4919673462d003c05123dd722
Maximos Nikiforakis <nikiforos@live.co.uk>
2024-09-13 19:48:41 +0300

    Adds transaction update form

--- transactions/urls.py
+++ transactions/urls.py
@@ -1,9 +1,24 @@
 from django.urls import path
 
-from transactions.views import index_transaction_view, index_transaction_categories_view
+from transactions.views import (
+    index_transaction_view,
+    index_transaction_categories_view,
+    update_transaction_category,
+    update_transaction_category_view,
+)
 
 urlpatterns = [
     path("", index_transaction_view, name="index-transactions-view"),
+    path(
+        "<int:transaction_id>/update/",
+        update_transaction_category_view,
+        name="update-transaction-category-view",
+    ),
+    path(
+        "<int:transaction_id>",
+        update_transaction_category,
+        name="update-transaction-category-endpoint",
+    ),
     path(
         "categories/",
         index_transaction_categories_view,
--- transactions/views.py
+++ transactions/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest
+from django.forms import Form, ChoiceField
 
 from transactions.models import Transaction, TransactionCategory
+from transactions.domain_models import TransactionCategoryEnum
 
 
 def index_transaction_view(request: HttpRequest):
@@ -13,10 +15,45 @@ def index_transaction_view(request: HttpRequest):
     )
 
 
+class UpdateTransactionCategoryForm(Form):
+    category_id = ChoiceField(choices=[(t.id, t.name) for t in TransactionCategoryEnum])
+
+
+def update_transaction_category_view(request: HttpRequest, transaction_id: int):
+    """GET /transactions/<int:transaction_id>/update/"""
+    transaction = Transaction.find(transaction_id)
+
+    if transaction is None:
+        return redirect("index-transactions-view")
+
+    context = {"form": UpdateTransactionCategoryForm(), "transaction": transaction}
+    return render(request, "transactions/update_form.html", context)
+
+
+def update_transaction_category(request: HttpRequest, transaction_id: int):
+    """POST /transactions/<int:transaction_id>/"""
+    transaction = Transaction.find(transaction_id)
+
+    if transaction is None:
+        return redirect("index-transactions-view")
+
+    category_id = request.POST["category_id"]
+
+    if TransactionCategory.find(category_id) is None:
+        return redirect("index-transactions-view")
+
+    transaction.category_id = category_id
+    transaction.save()
+    return redirect("index-transactions-view")
+
+
 def index_transaction_categories_view(request: HttpRequest):
-    """GET /transactions/categories"""
+    """GET /transactions/categories/"""
     return render(
         request,
         "transactions/categories/index.html",
         context={"categories": TransactionCategory.all(descending=True)},
     )
+
+
+def add_transaction_category_view(request: HttpRequest): ...
