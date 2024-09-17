649eb216afa89b29f4517c5509e90cd0d0a9d505
Maximos Nikiforakis <nikiforos@live.co.uk>
2024-09-17 15:32:50 +0300

    Refactors monthyear

--- utils/monthyear.py
+++ utils/monthyear.py
@@ -1,18 +1,38 @@
-from datetime import date
 from dataclasses import dataclass
+from calendar import month_name
+from datetime import date
 
 
 @dataclass
 class monthyear:
-    _date: date
+    month: int
+    year: int
 
-    @property
-    def month(self) -> int:
-        return self._date.month
+    def __init__(self, month, year):
+        try:
+            self.month = int(month)
+        except Exception:
+            raise TypeError(
+                f"could not create monthyear object with month: {month}; Expected integer. "
+            )
 
-    @property
-    def year(self) -> int:
-        return self._date.year
+        try:
+            self.year = int(year)
+        except Exception:
+            raise TypeError(
+                f"could not create monthyear object with year: {year}; Expected integer. "
+            )
+
+    def __repr__(self) -> str:
+        return f"{month_name[self.month]} {self.year}"
 
     def __hash__(self) -> int:
-        return hash(str(self.month) + str(self.year))
+        return hash(str(self.year) + str(self.month))
+
+    def __gt__(self, other) -> bool:
+        if not isinstance(other, monthyear):
+            raise TypeError(
+                f"> not supported between instances of 'monthyear' and {type(other)}"
+            )
+
+        return date(self.year, self.month, 1) > date(other.year, other.month, 1)
